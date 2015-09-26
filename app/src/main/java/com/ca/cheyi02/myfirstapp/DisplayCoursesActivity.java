d98cbc90ec640590257b1238c99e422332d1ced8
Thomas Cheng <cheyi02@ca.com>
2015-09-26 15:52:28 -0500

    use a postexecution to start an activity

--- app/src/main/java/com/ca/cheyi02/myfirstapp/DisplayCoursesActivity.java
+++ app/src/main/java/com/ca/cheyi02/myfirstapp/DisplayCoursesActivity.java
@@ -1,9 +1,7 @@
 package com.ca.cheyi02.myfirstapp;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,17 +10,8 @@ import android.widget.TextView;
 import android.content.Intent;
 import android.widget.Toast;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class DisplayCoursesActivity extends Activity {
-    private static final String TAG = DisplayCoursesActivity.class.getName();
-
     int imageRID(String imageName) {
         if (imageName.equals("three-com-1")) return R.drawable.threecom1;
         else if (imageName.equals("three-com-2")) return R.drawable.threecom2;
@@ -71,62 +60,10 @@ public class DisplayCoursesActivity extends Activity {
 
     }
 
-    private String convertInputStreamToString (InputStream inputStream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while ((line = bufferedReader.readLine()) != null) {
-            result += line;
-        }
-        if (null != inputStream) {
-            inputStream.close();
-        }
-        return result;
-    }
-
-    private String RetrieveLink(String urlString) {
-        InputStream inStream = null;
-        HttpURLConnection urlConnection = null;
-        Integer result=0;
-        String response="";
-
-        try {
-            URL url = new URL(urlString);
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-            // optional request header
-//            urlConnection.setRequestProperty("Content-Type", "text/html");
-//            urlConnection.setRequestProperty("Accept", "text/html");
-
-            urlConnection.setRequestMethod("GET");
-            int statusCode = urlConnection.getResponseCode();
-
-            if (statusCode == 200) {
-                inStream = new BufferedInputStream(urlConnection.getInputStream());
-                response = convertInputStreamToString(inStream);
-            }
-        } catch (Exception e) {
-            Log.d(TAG, "exception", e);
-        }
-        return response;
-    }
-
-    private class RetrieveLinkTask extends AsyncTask<String, Void, String> {
-        protected String doInBackground(String... urls) {
-            return RetrieveLink(urls[0]);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
-            Log.d("Download", result);
-        }
-    }
 
     void addCourseList() {
 
         Toast.makeText(getApplicationContext(), "hello", Toast.LENGTH_LONG).show();
-        new RetrieveLinkTask().execute(getResources().getString(R.string.course_end_point));
         addOneCourse("1001", "CA Identity Suite 101", "three-com-1");
         addOneCourse("1002", "CA Identity Suite 102", "three-com-2");
         addOneCourse("1003", "CA Identity Suite 103", "three-com-3");
--- app/src/main/java/com/ca/cheyi02/myfirstapp/MyActivity.java
+++ app/src/main/java/com/ca/cheyi02/myfirstapp/MyActivity.java
@@ -1,23 +1,92 @@
 package com.ca.cheyi02.myfirstapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MyActivity extends Activity {
     public final static String EXTRA_USERNAME = "com.ca.cheyi02.myfirstapp.USERNAME";
+    private static final String TAG = MyActivity.class.getName();
+
+    private String convertInputStreamToString (InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while ((line = bufferedReader.readLine()) != null) {
+            result += line;
+        }
+        if (null != inputStream) {
+            inputStream.close();
+        }
+        return result;
+    }
+
+    private String RetrieveLink(String urlString) {
+        InputStream inStream = null;
+        HttpURLConnection urlConnection = null;
+        Integer result=0;
+        String response="";
+
+        try {
+            URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // optional request header
+//            urlConnection.setRequestProperty("Content-Type", "text/html");
+//            urlConnection.setRequestProperty("Accept", "text/html");
+
+            urlConnection.setRequestMethod("GET");
+            int statusCode = urlConnection.getResponseCode();
+
+            if (statusCode == 200) {
+                inStream = new BufferedInputStream(urlConnection.getInputStream());
+                response = convertInputStreamToString(inStream);
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "exception", e);
+        }
+        return response;
+    }
+
+    private class RetrieveLinkTask extends AsyncTask <String, String, String> {
+        Context context;
+        private RetrieveLinkTask(Context context) {
+            this.context = context.getApplicationContext();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            return RetrieveLink(urls[0]);
+        }
+
+        protected void onPostExecute(String result) {
+            Intent intent = new Intent(context, DisplayCoursesActivity.class);
+
+            intent.putExtra(EXTRA_USERNAME, result);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+            Log.d("Download", result);
+        }
+    }
 
     public void login(View view) {
-        Intent intent = new Intent(this, DisplayCoursesActivity.class);
-
-        EditText usernameText = (EditText) findViewById(R.id.username_message);
-        String usernameString = usernameText.getText().toString();
-        intent.putExtra(EXTRA_USERNAME, usernameString);
-        startActivity(intent);
+        new RetrieveLinkTask(this).execute(getResources().getString(R.string.course_end_point));
     }
 
     @Override
